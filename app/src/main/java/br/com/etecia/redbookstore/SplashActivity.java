fc3dc8f677cd698047a6a0c480acf1c8394cd130
Kl3bY <caiovitorruas5@gmail.com>
2023-10-24 22:02:39 -0300

    splash

--- app/src/main/java/br/com/etecia/redbookstore/SplashActivity.java
+++ app/src/main/java/br/com/etecia/redbookstore/SplashActivity.java
@@ -2,7 +2,9 @@ package br.com.etecia.redbookstore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_layout);
+
+        //criando o carregamento da janela de login
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),
+                        MainActivity.class));
+                finish();//fechando a janela atual
+            }
+        },2000);
     }
 }
